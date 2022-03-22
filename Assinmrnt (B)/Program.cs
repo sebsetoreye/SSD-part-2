c07fcc26dcab1ee443d2a5ef5de2f03968db2ef2
sebas <sebas@DESKTOP-SEBASTI>
2022-03-22 11:39:51 +1100

    Add project files.

---
+++ Assinmrnt (B)/Program.cs
@@ -0,0 +1,36 @@
+﻿// stackoverflow used for the palendome 
+
+
+string sentence, fl;
+
+fl = "";
+
+Console.Write("Please enter your sentence : ");
+sentence = Console.ReadLine();
+
+Console.WriteLine(sentence.Length);
+
+if (sentence.Length < 20)
+{
+    Console.WriteLine("testing less then 20");
+
+    var fw = sentence;
+    fw = sentence.Substring(0, fw.IndexOf(' '));
+    Console.WriteLine(fw);
+}
+else if (sentence.Length >= 20)
+{
+    Console.WriteLine("testing for string between 20 & 40");
+
+    foreach (var part in sentence.Split(' '))
+    {
+        fl = part.Substring(0,1);
+        Console.WriteLine(fl);
+    }
+}
+else if (sentence.Length > 60)
+{
+    Console.WriteLine("testing this string is grater then 60");
+
+
+}
